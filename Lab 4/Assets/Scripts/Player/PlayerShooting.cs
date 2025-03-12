eb1bcf96a13d1ebee0817881aa1d32a8fe4d658c
Amy Roche <141393246+amezzles@users.noreply.github.com>
2025-03-12 20:59:48 +1100

    28 As a player, I want to receive audio feedback whenever I shoot a bullet so that I receive feedback that my inputs are valid (#51)
    ## Stories
    https://github.com/GDS1-Group-5-2025/lab4/issues/28
    https://github.com/GDS1-Group-5-2025/lab4/issues/9
    
    ## What does this PR do?
    - Adds sounds for shooting
    - Adds sounds for when there are no bullets
    - Adds sounds for reloading
    - Implemented reloading 
    - Added a placeholder UI to represent when the player is reloading
    - Added rate limit for shooting

--- Lab 4/Assets/Scripts/Player/PlayerShooting.cs
+++ Lab 4/Assets/Scripts/Player/PlayerShooting.cs
@@ -1,28 +1,92 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(AudioSource))]
 public class PlayerShooting : MonoBehaviour
 {
 
+    public int bulletCount = 10;
+    public float fireRate = 0.5f;
+    public float reloadTime = 2f;
+
+    public AudioClip shootSound;
+    public AudioClip reloadSound;
+    public AudioClip emptyGunSound;
+
+    public GameObject loadingImage;
+
+    private AudioSource _audioSource;
     private PlayerInput _playerInput;
     private InputActionMap _playerActionMap;
     private BulletManager _bulletManager;
 
+    private int _bullets;
+    private float _timeSinceLastShot;
+    private bool _isReloading;
+    private float _timeSinceReloadStart;
+
     private void Start()
     {
+        // Get audio source component
+        _audioSource = GetComponent<AudioSource>();
 
+        // Get Bullet Manager
         _bulletManager = FindFirstObjectByType<BulletManager>();
+        // Get player input component
         _playerInput = GetComponentInParent<PlayerInput>();
 
         // Get current player input action map
         _playerActionMap = _playerInput.currentActionMap;
+
+        // Set initial bullet count
+        _bullets = bulletCount;
+    }
+
+
+    private void Update()
+    {
+        // If player is reloading
+        if (_isReloading)
+        {
+            _timeSinceReloadStart += Time.deltaTime;
+            // If reload time has passed
+            if (_timeSinceReloadStart >= reloadTime)
+            {
+                // Reset bullets and reload flag
+                _bullets = bulletCount;
+                _isReloading = false;
+                _audioSource.PlayOneShot(reloadSound);
+                loadingImage.SetActive(false);
+            }
+        }
+
+        if (_timeSinceLastShot <= 1f / fireRate)
+        {
+            _timeSinceLastShot += Time.deltaTime;
+        }
     }
 
     public void Shoot(InputAction.CallbackContext ctx)
     {
-        if (ctx.action.actionMap == _playerActionMap && ctx.performed)
+        if (ctx.action.actionMap != _playerActionMap || !ctx.performed) return;
+        if (_timeSinceLastShot < 1f / fireRate) return;
+
+        if (_isReloading)
         {
-            _bulletManager.Shoot(transform.position + transform.up * 1f, transform.rotation);
+            // Play empty gun sound
+            _audioSource.PlayOneShot(emptyGunSound);
+            return;
+        }
+
+        _bulletManager.Shoot(transform.position + transform.up * 1f, transform.rotation);
+        _audioSource.PlayOneShot(shootSound);
+        _bullets--;
+        _timeSinceLastShot = 0f;
+        if (_bullets == 0)
+        {
+            loadingImage.SetActive(true);
+            _isReloading = true;
+            _timeSinceReloadStart = 0f;
         }
     }
 }
